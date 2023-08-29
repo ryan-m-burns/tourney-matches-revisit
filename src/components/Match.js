92e9c29bd1888116e1a212fc76c219913724a67d
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2023-08-28 20:53:07 -0700

    add multiple methods for conditional rendering: short-circuit evaluation & ternary operator

--- src/components/Match.js
+++ src/components/Match.js
@@ -8,12 +8,31 @@ function Match(props) {
       <h1>
         {players[0]} <span>vs</span> {players[1]}
       </h1>
-      {/* To be shown if there is a winner */}
+
+      {/* Ternary Operator Method */}
+      {winner ? (
+        <h2>
+          {winner} is the winner by {scoreDifference}!
+        </h2>
+      ) : (
+        <h2>No winner yet!</h2>
+      )}
+
+      {/* Short-Circuit Evaluation Method
+      {props.winner && (
+        <h2>
+          {props.winner} is the winner by {props.scoreDifference}!
+        </h2>
+      )}
+      {!props.winner && <h2>No winners yet!</h2>}
+      */}
+
+      {/* To be shown if there is a winner
       <h2>
         {winner} is the winner by {scoreDifference}!
       </h2>
-      {/* To be shown if there is no winner */}
-      <h2>No winner yet!</h2>
+      To be shown if there is no winner
+      <h2>No winner yet!</h2> */}
     </article>
   );
 }
