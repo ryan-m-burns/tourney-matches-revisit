bc518439362dbc84cf6710af220c077093698f2f
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2023-08-28 20:21:11 -0700

    pass down props to Match

--- src/components/MatchList.js
+++ src/components/MatchList.js
@@ -3,12 +3,16 @@ import Match from "./Match";
 import matchData from "../data/matchData";
 
 function MatchList(props) {
+  const oneMatch = matchData[0];
+
   return (
     <section className="PlayerList MatchList">
       <h1> Current Matches </h1>
-      <Match />
+      <Match
+        players={oneMatch.players}
+        winner={oneMatch.winner}
+        scoreDifference={oneMatch.scoreDifference}
+      />
     </section>
   );
 }
-
-export default MatchList;
