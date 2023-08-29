bd2cd2a8592c48f7eefe95983ac5c7c09a0a412e
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2023-08-28 20:04:37 -0700

    set up helpers. Write preparePlayerData and addWinsToPlayers functions

---
+++ src/helpers/playerHelpers.js
@@ -0,0 +1,11 @@
+// input: playerData
+export const preparePlayerData = (playerData) => {
+  return Object.values(playerData);
+};
+// output: [{player}, {player}]
+
+// input: playerDataArray, matchData
+export const addWinsToPlayer = (playerDataArray, matchData) => {
+
+};
+// output: [{player}, {player}] (each player having a win key and a numerical value)
