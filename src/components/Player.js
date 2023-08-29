2c607a3a311e5e46d007951cda6ed01e2753c0be
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2023-08-28 20:42:07 -0700

    use props in Player component

--- src/components/Player.js
+++ src/components/Player.js
@@ -6,14 +6,14 @@ function Player(props) {
   return (
     <article className="Player">
       <h1>
-        John <span>Secret</span> Doe
+        {firstName} <span>{gamerTag}</span> {lastName}
       </h1>
       {/* To be shown when there is no wins */}
       <h2 className="zero">Currently with no wins :(</h2>
       {/* To be shown when there is 1 win */}
-      <h2>Currently at 1 win</h2>
+      <h2>Currently at {wins} win</h2>
       {/* To be shown when there is more than 1 win */}
-      <h2>Currently at 1+ wins</h2>
+      <h2>Currently at {wins} wins</h2>
     </article>
   );
 }
