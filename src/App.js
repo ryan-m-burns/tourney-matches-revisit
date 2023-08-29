708cf8a7695f9e69fe0e8cde9437dab3918b0617
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2023-08-28 19:38:40 -0700

    import MatchList and PlayerList into App.js and display components in the app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 //import logo from './logo.svg';
 import './App.css';
-
+import PlayerList from './components/PlayerList';
+import MatchList from './components/MatchList';
+import Player from './components/Player';
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <h1>
+        Tournament Matches{" "}
+        <span>Where Coding and Tournaments found their Match!</span>
+      </h1>
+      <PlayerList />
+      <MatchList />
+    </div>
   );
 }
 
