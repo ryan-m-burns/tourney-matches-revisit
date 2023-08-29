15fb66a302f73b3090158f59cc18edb0e60228e8
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2023-08-28 20:58:09 -0700

    add conditional rendering to Player component

--- src/components/Player.js
+++ src/components/Player.js
@@ -8,12 +8,19 @@ function Player(props) {
       <h1>
         {firstName} <span>{gamerTag}</span> {lastName}
       </h1>
-      {/* To be shown when there is no wins */}
+
+      {wins === 0 && <h2 className="zero">Currently with no wins :(</h2>}
+      {wins === 1 && <h2>Currently at {wins} win</h2>}
+      {wins > 1 && <h2>Currently at {wins} win</h2>}
+
+      {/* 
+       To be shown when there is no wins 
       <h2 className="zero">Currently with no wins :(</h2>
-      {/* To be shown when there is 1 win */}
+       To be shown when there is 1 win 
       <h2>Currently at {wins} win</h2>
-      {/* To be shown when there is more than 1 win */}
-      <h2>Currently at {wins} wins</h2>
+       To be shown when there is more than 1 win 
+      <h2>Currently at {wins} wins</h2> 
+      */}
     </article>
   );
 }
