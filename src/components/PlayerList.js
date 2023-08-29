0ea7484d93c40a1a0d4be617217c69bf951481d1
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2023-08-28 20:13:24 -0700

    complete helper file. import and use in PlayerList

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -2,8 +2,11 @@ import React from "react";
 import Player from "./Player";
 import playerData from "../data/playerData";
 import matchData from "../data/matchData";
+import { preparePlayerData, addWinsToPlayer } from "../helpers/playerHelpers";
 
 function PlayerList(props) {
+  const playerDataArray = preparePlayerData(playerData);
+  const parsedPlayerData = addWinsToPlayer(playerDataArray, matchData);
   return (
     <section className="PlayerList">
       <h1>Current Participating Players</h1>
